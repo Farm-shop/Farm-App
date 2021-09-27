2f8a3b34795394fd1464025e0e3a223e738f0ddf
haneenbader <haneenalwatan993@gmail.com>
2021-09-27 23:00:37 +0300

    css

--- FARMSHOP/app/src/main/java/com/example/farmshop/FarmActivity.java
+++ FARMSHOP/app/src/main/java/com/example/farmshop/FarmActivity.java
@@ -33,41 +33,41 @@ public class FarmActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Button addProduct=findViewById(R.id.addProduct);
-        addProduct.setOnClickListener((v)->{
-            Intent intent=new Intent(FarmActivity.this,AddProduct.class);
-            startActivity(intent);
-        });
+//        Button addProduct=findViewById(R.id.addProduct);
+//        addProduct.setOnClickListener((v)->{
+//            Intent intent=new Intent(FarmActivity.this,AddProduct.class);
+//            startActivity(intent);
+//        });
 
         renderOfProduct();
     }
     private void renderOfProduct(){
-        RecyclerView allProductRecyclerView=findViewById(R.id.recyclerViewProductOfFarm);
-        Handler handler = new Handler(Looper.getMainLooper(),
-                new Handler.Callback() {
-                    @Override
-                    public boolean handleMessage(@NonNull Message msg) {
-                        allProductRecyclerView.getAdapter().notifyDataSetChanged();
-                        return false;
-                    }
-                });
-        ArrayList<Product> allProduct=new ArrayList<Product>();
-        Amplify.API.query(
-                ModelQuery.list(Farm.class),
-                response -> {
-                    for (Farm farm : response.getData()) {
-                        if (farm.getUserSignId().equals(Amplify.Auth.getCurrentUser().getUserId())){
-                            Log.i("MyAmplifyApp", farm.getName());
-                            for (Product product : farm.getProducts()) {
-                                allProduct.add(product);
-                            }
-                        }
-                    }
-                    handler.sendEmptyMessage(1);
-                },
-                error -> Log.e("MyAmplifyApp", "Query failure", error)
-        );
-        allProductRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
-        allProductRecyclerView.setAdapter(new ProductAdapter(allProduct));
+//        RecyclerView allProductRecyclerView=findViewById(R.id.recyclerViewProductOfFarm);
+//        Handler handler = new Handler(Looper.getMainLooper(),
+//                new Handler.Callback() {
+//                    @Override
+//                    public boolean handleMessage(@NonNull Message msg) {
+//                        allProductRecyclerView.getAdapter().notifyDataSetChanged();
+//                        return false;
+//                    }
+//                });
+//        ArrayList<Product> allProduct=new ArrayList<Product>();
+//        Amplify.API.query(
+//                ModelQuery.list(Farm.class),
+//                response -> {
+//                    for (Farm farm : response.getData()) {
+//                        if (farm.getUserSignId().equals(Amplify.Auth.getCurrentUser().getUserId())){
+//                            Log.i("MyAmplifyApp", farm.getName());
+//                            for (Product product : farm.getProducts()) {
+//                                allProduct.add(product);
+//                            }
+//                        }
+//                    }
+//                    handler.sendEmptyMessage(1);
+//                },
+//                error -> Log.e("MyAmplifyApp", "Query failure", error)
+//        );
+//        allProductRecyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),3));
+//        allProductRecyclerView.setAdapter(new ProductAdapter(allProduct));
     }
 }
